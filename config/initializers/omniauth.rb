a2b7ce9e67d5665f382051b861dfac1c923c254b
Cristina <cristina@conekta.mx>
2012-12-05 14:32:10 -0600

    mes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require "omniauth"
 require "omniauth-salesforce"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
+  provider :salesforce, "3MVG9y6x0357Hlef6GCKmow8zOl8h5otKPc2BYHrUhETUs43QhnXiAsCaeeIiH22Y0PaQ3N.q9Nu28Tr0k_zW", "1136101935488826437"
 end
 
 
