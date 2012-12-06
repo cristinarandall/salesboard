76c1eb803f04c05e8ffeb35ebf04ad8b9e30de7d
Cristina <cristina@conekta.mx>
2012-12-06 09:34:49 -0600

    mes

--- db/migrate/20121129214325_add_user.rb
+++ db/migrate/20121129214325_add_user.rb
@@ -7,6 +7,9 @@ class AddUser < ActiveRecord::Migration
       t.timestamps
   end
 
+#add_column :users, :persistence_token, :string
+#add_column :users, :password_salt, :string
+#add_column :users, :crypted_password, :string
 
 
   end
