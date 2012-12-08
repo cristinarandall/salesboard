39af33fb3be978c4a2d237081be2371b0ffeeef6
Cristina <cristina@conekta.mx>
2012-12-07 19:28:21 -0600

    mes

--- db/migrate/20121129214325_add_user.rb
+++ db/migrate/20121129214325_add_user.rb
@@ -7,7 +7,7 @@ class AddUser < ActiveRecord::Migration
 #      t.timestamps
 #  end
 
-add_column :authentications, :secret, :string
+add_column :authentications, :instance_url, :string
   end
 
   def down
