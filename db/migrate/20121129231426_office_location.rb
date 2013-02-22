f3528d5a27849a43e4fca895564be178ba49d6d0
Cristina <cristina@conekta.mx>
2013-02-21 22:33:33 -0600

    mes

--- db/migrate/20121129231426_office_location.rb
+++ db/migrate/20121129231426_office_location.rb
@@ -6,7 +6,7 @@ class OfficeLocation < ActiveRecord::Migration
 #      t.timestamps
 #  end
 
-add_column :sales_person, :manager_id, :int
+add_column :users, :manager_id, :int
 add_column :office_locations, :manager_id, :int
 
 add_column :users, :company, :string
