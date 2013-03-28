89807ed46853d1b692958d51191ec7a3fc022ae1
Cristina <cristina@conekta.mx>
2013-03-28 13:19:23 -0600

    Mes

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 class DataPoint
-#    include MongoMapper::Document
-  include Mongoid::Document
-  include Mongoid::Timestamps
+    include MongoMapper::Document
+#  include Mongoid::Document
+#  include Mongoid::Timestamps
 
 #    field :user_id, type: Integer
 #    field :topic_id, type: Integer
