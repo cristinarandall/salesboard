3ce06be52aaf977573b8782c6797596b6fe92a8d
Cristina <cristina@conekta.mx>
2013-03-28 13:24:44 -0600

    mes

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -3,9 +3,17 @@ class DataPoint
     include MongoMapper::Document
 #  include Mongoid::Document
 #  include Mongoid::Timestamps
+  plugin MongoMapper::Plugins::Timestamps
 
 #    field :user_id, type: Integer
 #    field :topic_id, type: Integer
+    key :user_id, Integer
+    key :topic_id, Integer
+
+
+    key :quantity, Integer
+    timestamps!
+
 
  #   field :quantity, type: Integer
   #  field :created_at, type: DateTime
